b08311d58009188b3fe65175c6258617ebba7fb6
Michelle Rios <brendamichelle995@gmail.com>
2021-05-04 15:11:05 -0400

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,23 +3,21 @@ Ride.destroy_all
 Passenger.destroy_all
 Driver.destroy_all
 
-puts "Seeding passengers..."
-# Create sample passengers
-50.times do
+puts "🙋‍♀️🙋‍♀️🙋‍♀️ Seeding passengers 🙋‍♀️🙋‍♀️🙋‍♀️..."
+20.times do
   Passenger.create(first_name: Faker::Name.name,
                    last_name: Faker::Name.last_name,
-                   rating: rand(1...5).to_f,
+                   rating: rand(1...5),
                    email: Faker::Internet.email,
                    cell_number: Faker::PhoneNumber.cell_phone)
 end
 
-puts "Seeding drivers..."
-# Create 10 random Drivers
-50.times do
+puts "✇✇✇ Seeding drivers ✇✇✇..."
+20.times do
   Driver.create(first_name: Faker::Name.name,
                 last_name: Faker::Name.name,
                 email: Faker::Internet.email,
-                rating: rand(1...5).to_f,
+                rating: rand(1...5),
                 car: Faker::Vehicle.make_and_model,
                 car_color: Faker::Vehicle.color,
                 license_plate: Faker::Vehicle.license_plate,
@@ -29,20 +27,17 @@ end
 
 puts "🚗🚗🚗 Seeding rides 🚗🚗🚗..."
 # Create 20 random rides
-70.times do
-  # Get a driver instance:
-  driver_id = Driver.ids.sample
-  # Get a passenger instance:
-  passenger_id = Passenger.ids.sample
-  # Get a random price between 1 and 1000:
-  price = rand(1..1000).to_f
+50.times do
+  driver_id = Driver.ids.sample # Get a driver instance
+  passenger_id = Passenger.ids.sample # Get a passenger instance
+  price = rand(1.0..1000.0)   # Get a random price between 1 and 1000
 
   # TODO: create rides! Remember, a ride belongs to a driver
   # and a ride belongs to a passenger. Driver -< Ride >- Passenger
 
-  # Use Faker for some fake pick up/drop off data:
+  # Feel free to use Faker for some fake pick up/drop off address data:
   # https://github.com/faker-ruby/faker/blob/master/doc/default/address.md
 
 end
 
-puts "Done!"
+puts "Done seeding! 🌸"
